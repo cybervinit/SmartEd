5dec65bb5074fde23c039e959b4fa8a60d7b1770
Vinit Soni <vinitsoni357@gmail.com>
2017-01-07 13:37:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/educationmaster/smarted/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.educationmaster.smarted;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
